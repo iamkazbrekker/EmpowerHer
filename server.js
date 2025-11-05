d7f7bbc4eb26c358c05d184c0c2ddb5a3a264b9a
Sammy K. <kazbrekker898@gmail.com>
2025-11-05 16:12:09 +0530

    cleaned up the server file

--- server.js
+++ server.js
@@ -14,47 +14,12 @@ const PORT = 8000;
 const dataPath = path.join(__dirname, 'data', 'testimonials.json');
 const dbPath = path.join(__dirname, 'empowerher.db');
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-const server = createServer(async (req, res) => {
-  try {
-    if (req.url !== '/api'){
-      const pathToResources = redirectTo(req, __dirname)
-      const content = await fs.readFile(pathToResources)
-      const contentType = getContentType(pathToResources)
-
-      res.setHeader('Content-Type', contentType)
-      res.end(content)
-      return
-    }else{
-      if (req.url.startsWith('/api')){
-        if (req.method === 'GET'){
-          const data = await fs.readFile(dataPath, 'utf-8')
-          res.setHeader('Content-Type', 'application/json')
-          res.end(data)
-          return
-        }
-      }
-    }
-  } catch (err) {
-      console.log(err)
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain')
-      res.end('404: File not found')
-    }
-=======
-=======
->>>>>>> Stashed changes
 // ---------- SQLite Setup ----------
 let db;
 (async () => {
   db = await open({
     filename: dbPath,
     driver: sqlite3.Database
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   });
 
   await db.exec(`
